a6a2df7d0b7f239b1a5959b71ec37a0014436e40
melwyn95 <melwyn95@gmail.com>
2019-07-20 23:07:54 +0530

    Fix Bug related to hardcode pathname

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+function getNewPathName() {
+  const urlParams = window.location.pathname.split('/');
+  urlParams[urlParams.length - 1] = +urlParams[urlParams.length - 1] + 1;
+  return urlParams.join('/');
+}
 
 function Box({ color, top, left, count }) {
   return <Fragment>
     {count == 3 && <div style={{position: "absolute", backgroundColor: color, height: "50%", width: "50%", top: `${top}vh`, left: `${left}vw`}} />}
-    {count < 3 && <iframe title={`${count}`} src={`/${+count+1}`} style={{position: "absolute", height: "50%", width: "50%", top: `${top}vh`, left: `${left}vw`}} frameBorder="0"/>}
+    {count < 3 && <iframe title={`${count}`} src={`${getNewPathName()}`} style={{position: "absolute", height: "50%", width: "50%", top: `${top}vh`, left: `${left}vw`}} frameBorder="0"/>}
   </Fragment>;
 }
 
 function Container(props) {
   const { match: { params: { count = '0' } } } = props;
-  console.log(count);
   return (<div style={{position: "absolute", height:"100vh", width: "100vw"}}>
     <Box color="black" top={0} left={0} count={count}/>
     <Box color="white" top={0} left={50} count={count}/>
