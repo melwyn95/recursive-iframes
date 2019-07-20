68a023faa60a2e69245935f9a83d8f39e43bb6d3
melwyn95 <melwyn95@gmail.com>
2019-07-20 22:36:41 +0530

    Updates

---
+++ precache-manifest.10e6e27193b7a10e6fc9934781ed9127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ef19cd188f841b8531abb5726d368a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f56758c9e40b18b6ba",
+    "url": "/static/css/main.d7e09297.chunk.css"
+  },
+  {
+    "revision": "5f9edcf325c41ffa7c24",
+    "url": "/static/js/2.1403189c.chunk.js"
+  },
+  {
+    "revision": "48f56758c9e40b18b6ba",
+    "url": "/static/js/main.e5175191.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
